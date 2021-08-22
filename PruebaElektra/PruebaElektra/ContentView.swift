00bedd316333e8d4817eba87a48158420f42f5bd
NarcisoMB <89325087+NarcisoMB@users.noreply.github.com>
2021-08-21 19:49:34 -0500

    Creación Proyecto

---
+++ PruebaElektra/PruebaElektra/ContentView.swift
@@ -0,0 +1,19 @@
+//  ContentView.swift
+//  PruebaElektra
+//  Created by Narciso Meza on 21/08/21.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("Hellow")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PruebaElektra/PruebaElektra/PruebaElektraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaElektraApp.swift
+//  PruebaElektra
+//
+//  Created by Narciso Meza on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PruebaElektraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
